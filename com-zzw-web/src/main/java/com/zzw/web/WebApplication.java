2b9e56d82cee2bb730792b1b0e5eecc54477305f
Administrator <Administrator@SKY-20170422YGJ>
2017-06-25 21:53:25 +0800

    ddd

--- com-zzw-web/src/main/java/com/zzw/web/WebApplication.java
+++ com-zzw-web/src/main/java/com/zzw/web/WebApplication.java
@@ -2,6 +2,10 @@ package com.zzw.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * 启动类.shiro模块不支持devtools
@@ -11,8 +15,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @version v.0.1
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
+	}
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 }
--- com-zzw-web/src/main/java/com/zzw/web/controller/UserInfoController.java
+++ com-zzw-web/src/main/java/com/zzw/web/controller/UserInfoController.java
@@ -3,20 +3,36 @@ package com.zzw.web.controller;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.zzw.web.domain.User;
+
+
  
 @Controller
 @RequestMapping("/userInfo")
 public class UserInfoController {
 	private Logger logger =  LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
     /**
      * 用户查询.
      * @return
      */
-    @RequestMapping("/userList")
-    public String userInfo(){
+    @RequestMapping("/userList/{id}")
+    public String userInfo(@PathVariable("id") Long id,ModelMap model){
        logger.info("日志输出测试 Debug==>userInfo");
+       User user = restTemplate.getForObject("http://COM-ZZW-BASEDATA-SERVICE/"+id, User.class);
+   	   System.out.println(user.getUsername());
+   	   model.put("user", user);
        return "userInfo";
     }
    
@@ -27,6 +43,8 @@ public class UserInfoController {
     @RequestMapping("/userAdd")
     @RequiresPermissions("userinfo:add")//权限管理;
     public String userInfoAdd(){
+    	//User list = restTemplate.getForObject("http://COM-ZZW-BASEDATA-SERVICE/list", User.class);
+    	//System.out.println(list.getUsername());
     	System.out.println("userInfoAdd=>>$$$");
        return "userInfoAdd";
     }
