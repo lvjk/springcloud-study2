1a6f8f55b4008ef1bd649330c7edb38e8a19c392
Administrator <Administrator@SKY-20170422YGJ>
2017-07-02 16:39:35 +0800

    11

--- com-zzw-web/src/main/java/com/zzw/web/controller/UserInfoController.java
+++ com-zzw-web/src/main/java/com/zzw/web/controller/UserInfoController.java
@@ -3,7 +3,6 @@ package com.zzw.web.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
